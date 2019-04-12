87f271fbd363339bd7ffc58962017d2aca05e617
Paulo Nascimento <zippy.zippy49@gmail.com>
2019-04-11 20:33:24 -0700

    updating database

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-convex-32847',
     user : 'postgres',
-    password : 'datadb390*',
+    password : '',
     database : 'smart-brain'
   }
 });
